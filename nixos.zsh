c6e3ff33639b114be6e8e769fc00949b6bb27d92
Josip Tomurad <tomurad.josip@gmail.com>
2026-02-03 20:21:06 +0100

    update nixos aliases

--- nixos.zsh
+++ nixos.zsh
@@ -1,6 +1,6 @@
 #### NixOS aliases
 
 # Rebuild and switch to the new configuration
-alias rb="nix flake update aliases --flake \$HOME/nixosconfig && sudo nixos-rebuild switch --impure --flake \$HOME/nixosconfig#\$(hostname)"
+alias rb="nix flake update aliases --flake \$NIXOS_FLAKE && sudo nixos-rebuild switch --impure --flake \$NIXOS_FLAKE#\$(hostname)"
 # Rebuild and test the new configuration (doesn't persist across reboot)
-alias rbt="nix flake update aliases --flake \$HOME/nixosconfig && sudo nixos-rebuild test --impure --flake \$HOME/nixosconfig#\$(hostname)"
+alias rbt="nix flake update aliases --flake \$NIXOS_FLAKE && sudo nixos-rebuild test --impure --flake \$NIXOS_FLAKE#\$(hostname)"
