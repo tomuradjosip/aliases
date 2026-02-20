5469f0555caa3223cb3ae88f4dcee6aafc2c918e
Josip Tomurad <tomurad.josip@gmail.com>
2026-02-20 15:21:57 +0100

    update homelab aliases

--- homelab.zsh
+++ homelab.zsh
@@ -7,10 +7,14 @@ alias hstatus="/containers/homelab/scripts/status-homelab.sh"
 # Sync pihole DNS
 alias hpihole="/containers/homelab/scripts/sync-pihole-dns.py"
 # Restart homelab target
-alias hrestart="systemctl --user restart homelab.target"
+alias hreboot="systemctl --user restart homelab.target"
 # Restart specific homelab service
-alias hrestart-service="systemctl --user restart"
+alias hrestart="systemctl --user restart"
+# Start specific homelab service
+alias hstart="systemctl --user start"
+# Stop specific homelab service
+alias hstop="systemctl --user stop"
 # View homelab journal logs
-alias hjournal="journalctl -e --user-unit"
+alias hjournal="journalctl -e --no-pager --user-unit"
 # Go to media directory
 alias mcd="cd /bulk/media"
