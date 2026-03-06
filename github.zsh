51b03c500243af8957c4e1a084ef12032b894b29
Josip Tomurad <josip.tomurad@happening.xyz>
2026-03-06 22:22:17 +0100

    add github aliases

---
+++ github.zsh
@@ -0,0 +1,10 @@
+#### GitHub CLI aliases
+
+# Create pull request
+alias pull="gh pr create --fill"
+# View pull request
+alias brpr="gh pr view --web"
+# Merge pull request
+alias merge="gh pr merge --delete-branch --merge && cleanup"
+# Browse repository
+alias browse="gh browse"
